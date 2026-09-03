76df377b5487aaa649724459018820e6b41f551d
Mahammadali Jafarov <mahammadali.jafarov1@gmail.com>
2026-09-03 17:47:39 +0400

    update

---
+++ fundamentals/Day_15/main.py
@@ -0,0 +1,54 @@
+
+def calculate_total(price,tax_rate=0.05):
+
+    '''
+    Default and Positional Arguments: Write a function calculate_total(price, tax_rate=0.05). 
+    It must accept price as a positional argument and return the price plus the calculated tax.
+    '''
+            
+
+def extract_coordinates(coord_tuple):
+
+    '''
+    Packing and Unpacking (Tuple): Write a function extract_coordinates(coord_tuple). 
+    The function must unpack a tuple of three elements (x, y, z) into separate variables, 
+    sum them, and return the result.
+    '''
+
+def create_profile(first_name,last_name,age):
+
+    '''
+    Keyword Arguments: Write a function create_profile(first_name, last_name, age). 
+    Call this function by explicitly naming each argument in reverse order (e.g., passing age first) 
+    to return a formatted string: "Name: [first_name] [last_name], Age: [age]".
+    '''
+
+def filter_and_square(number_list):
+
+    '''
+    Built-in Functions: Write a function filter_and_square(numbers_list). 
+    Use the built-in filter() function to extract only the even numbers, and the map() function to square them. 
+    Return the result as a list.
+    '''
+
+def  incremnet_counter():
+
+    '''
+    Variable Scope (Global vs Local): Define a global variable counter = 0. 
+    Write a function increment_counter() that uses the global keyword to modify the variable, 
+    incrementing it by 1, and returning the new value.
+    '''
+
+def sum_all(*args):
+
+    '''
+    Arbitrary Arguments (*args): Write a function sum_all(*args) 
+    that accepts any number of positional integer arguments. 
+    It must iterate through the arguments and return their total sum. 
+    Do not use the built-in sum() function.
+    '''
+
+
+if __name__=="__main__":
+
+    pass
